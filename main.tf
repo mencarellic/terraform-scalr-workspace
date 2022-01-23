913172b397be1ee75320a127d1d9b61063e7c575
Carlo Mencarelli <carlo.mencarelli@billgo.com>
2022-01-22 22:15:05 -0700

    Workspaces

--- main.tf
+++ main.tf
@@ -1,5 +1,29 @@
 resource "scalr_environment" "test" {
   name                    = "Workspace"
-  account_id              = var.account_id
+  account_id              = var.account-id
   cost_estimation_enabled = true
+}
+
+resource "scalr_workspace" "aws-development" {
+  name              = "aws-development"
+  environment_id    = scalr_envrionment.test.id
+  terraform_version = var.terraform-version
+}
+
+resource "scalr_workspace" "azure-development" {
+  name              = "azure-development"
+  environment_id    = scalr_envrionment.test.id
+  terraform_version = var.terraform-version
+}
+
+resource "scalr_workspace" "aws-production" {
+  name              = "aws-production"
+  environment_id    = scalr_envrionment.test.id
+  terraform_version = var.terraform-version
+}
+
+resource "scalr_workspace" "azure-production" {
+  name              = "azure-production"
+  environment_id    = scalr_envrionment.test.id
+  terraform_version = var.terraform-version
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,11 @@
-variable "account_id" {
+variable "account-id" {
   type        = string
   description = "Scalr account ID"
   default     = ""
+}
+
+variable "terraform-version" {
+  type        = string
+  description = "Global Terraform version to use"
+  default     = "1.1.3"
 }
