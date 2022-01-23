73f4fc672b0a879115042a008e7b6680fb22d0cb
Carlo Mencarelli <carlo.mencarelli@billgo.com>
2022-01-22 22:16:09 -0700

    Fix typo

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "scalr_environment" "test" {
+resource "scalr_environment" "workspace" {
   name                    = "Workspace"
   account_id              = var.account-id
   cost_estimation_enabled = true
@@ -6,24 +6,24 @@ resource "scalr_environment" "test" {
 
 resource "scalr_workspace" "aws-development" {
   name              = "aws-development"
-  environment_id    = scalr_envrionment.test.id
+  environment_id    = scalr_environment.workspace.id
   terraform_version = var.terraform-version
 }
 
 resource "scalr_workspace" "azure-development" {
   name              = "azure-development"
-  environment_id    = scalr_envrionment.test.id
+  environment_id    = scalr_environment.workspace.id
   terraform_version = var.terraform-version
 }
 
 resource "scalr_workspace" "aws-production" {
   name              = "aws-production"
-  environment_id    = scalr_envrionment.test.id
+  environment_id    = scalr_environment.workspace.id
   terraform_version = var.terraform-version
 }
 
 resource "scalr_workspace" "azure-production" {
   name              = "azure-production"
-  environment_id    = scalr_envrionment.test.id
+  environment_id    = scalr_environment.workspace.id
   terraform_version = var.terraform-version
 }
